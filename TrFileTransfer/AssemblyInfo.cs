775123857aa7aad18593cb1eff33ead3a46d5d8c
54gogogo10 <gogogo10@163.com>
2026-09-09 12:36:18 +0800

    chore: 版本升至 2.10.0.0

--- TrFileTransfer/AssemblyInfo.cs
+++ TrFileTransfer/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("TrFileTransfer")]
 [assembly: AssemblyProduct("TrFileTransfer")]
-[assembly: AssemblyVersion("2.9.0.0")]
-[assembly: AssemblyFileVersion("2.9.0.0")]
+[assembly: AssemblyVersion("2.10.0.0")]
+[assembly: AssemblyFileVersion("2.10.0.0")]
